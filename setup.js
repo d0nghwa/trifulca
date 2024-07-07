6881ef374d32938f36ec64d8fd824020ae0586b3
d0nghwa <donghwa.kim17@gmail.com>
2024-07-07 02:05:17 -0700

    Finished piece movement, piece finishing, and added piece icons

--- setup.js
+++ setup.js
@@ -1,3 +1,5 @@
+import { Trifulca } from "./trifulca.js";
+
 const grid_matrix = [
     [1, 0, 1, 0, 1],
     [0, 0, 0, 0, 0],
@@ -8,7 +10,7 @@ const grid_matrix = [
     [1, 0, 1, 0, 1]
 ];
 
-function init_grid(matrix) {
+function initGrid(matrix) {
     let grid = document.createElement('table');
     grid.className = 'grid';
     for (let r = 0; r < matrix.length; r++) {
@@ -17,16 +19,23 @@ function init_grid(matrix) {
             let tile = grid_row.appendChild(document.createElement('td'));
             tile.className = matrix[r][c] ? 'dark tile' : 'light tile';
             tile.id = [r, c];
+
+            let overlay = tile.appendChild(document.createElement('div'));
+            overlay.className = 'overlay';
+            overlay.id = [r, c] + ' overlay';
         }
     }
     return grid;
 }
 
-function start_game() {
+function startGame() {
     const game = new Trifulca();
 }
 
-let grid = init_grid(grid_matrix);
+let grid = initGrid(grid_matrix);
 document.getElementById('content').appendChild(grid);
 
-start_game();
+let button = document.getElementById('startButton');
+button.addEventListener("click", startGame, { once: true });
+
+
