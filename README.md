d1e939425f3823c2f2dc207d19699ec197152759
d0nghwa <donghwa.kim17@gmail.com>
2024-07-24 23:04:07 -0700

    refactor: separated UI and game state code

--- README.md
+++ README.md
@@ -1,13 +1,1 @@
 # trifulca
-
-start game
-start turn (red first)
-wait for input
-end turn
-check game -> not done
-next turn
-start turn (white)
-wait for input
-...
-check game -> done
-end game
--- tests/trifulcaPiece.test.js
+++ tests/trifulcaPiece.test.js
@@ -1,5 +1,15 @@
-import { Conquistador, Dame, Knight } from "./../code/trifulcaPiece.js";
-import { coordEquals } from "./utils.js";
+import { Piece } from "./../src/trifulcaPiece.js";
+
+function isEqual(a, b) {
+    if (a === b) return true;
+    if (a.length !==  b.length) return false;
+    
+    for (let i = 0; i < a.length; i++) {
+        if (a[i].r !== b[i].r || a[i].c !== b[i].c)
+            return false;
+    }
+    return true;
+}
 
 test('test moveable tiles basic', () => {
     let board = [];
@@ -10,18 +20,40 @@ test('test moveable tiles basic', () => {
         while (row.push(null) < 5); 
     }
     
-    const conq = new Conquistador([0,0], 'white');
+    const conq = new Piece(
+        {r: 0, c: 0}, 
+        'WHITE', 
+        'CONQ'
+    );
     const conq_moves = conq.getMoveableTiles(board);
-    expect(coordEquals(conq_moves, [[0,2],[2,0]])).toBeTruthy();
-    expect(coordEquals(conq_moves, [[2,0],[0,2]])).toBeTruthy();
-    expect(coordEquals(conq_moves, [[0,2]])).toBeFalsy();
+    expect(isEqual(conq_moves, [
+        {r: 2, c: 0},
+        {r: 0, c: 2}
+    ])).toBeTruthy();
+    expect(isEqual(conq_moves, [
+        {r: 0, c: 2}
+    ])).toBeFalsy();
 
-    const nite = new Knight([0,4], 'white');
+    const nite = new Piece(
+        {r: 0, c: 4}, 
+        'WHITE', 
+        'NITE'
+    );
     const nite_moves = nite.getMoveableTiles(board);
-    expect(coordEquals(nite_moves, [[1,3]])).toBeTruthy();
-    expect(coordEquals(nite_moves, [])).toBeFalsy();
+    expect(isEqual(nite_moves, [
+        {r: 1, c: 3}
+    ])).toBeTruthy();
+    expect(isEqual(nite_moves, [])).toBeFalsy();
 
-    const dame = new Dame([6,2], 'red');
+    const dame = new Piece(
+        {r: 6, c: 2},
+        'RED', 
+        'DAME'
+    );
     const dame_moves = dame.getMoveableTiles(board);
-    expect(coordEquals(dame_moves, [[6,1],[6,3],[5,2]])).toBeTruthy();
+    expect(isEqual(dame_moves, [
+        {r: 5, c: 2},
+        {r: 6, c: 1},
+        {r: 6, c: 3}
+    ])).toBeTruthy();
 })
